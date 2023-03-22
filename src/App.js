41d45747ea3ed2d5498584d55640e69c818c633d
Reed Keyes <rbkeyes@gmail.com>
2023-03-22 11:46:34 -0700

    changed my mind, will just use Header instead of container

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Container from './components/Container';
+import Header from './components/Header';
 
 function App() {
-  return <Container />;
+  return <Header />;
 }
 
 export default App;
