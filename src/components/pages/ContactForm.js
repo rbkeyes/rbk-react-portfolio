07a1e4464a33f1fe29d1a24ad77cdafc38053d39
Reed Keyes <rbkeyes@gmail.com>
2023-03-25 12:05:04 -0700

    contact form basic formatting, bootstrap styling added

---
+++ src/components/pages/ContactForm.js
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+
+// contact form takes in props
+function ContactForm(props) {
+  // useState variables for name, email, and form message
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  // useState variable to set valid true/false
+  // const [validation, isValid] = useState('');
+
+  // handle form submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // 🔮 on submit - will add when I have a backend
+    // props.onSubmit({
+    //   id: Math.random(Math.floor() * 1000),
+    //   name: name,
+    //   email: email,
+    //   message: message
+    // });
+
+    // reset state to "" for form fields on submit
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleMessage = (e) => {
+    setMessage(e.target.value);
+  };
+
+  // const handleInputValidation = (e) => {
+  //   e.target.value ? isValid(true) : isValid(false);
+  // };
+
+
+const styles = {
+  textarea: {
+    height: "10rem"
+  }
+}
+
+  return (
+    <div class="d-flex justify-content-around align-items-start p-4">
+    <form className="contact-form col-md-4" onSubmit={handleSubmit}>
+      <div className="form-floating mb-3">
+        <input
+          className="form-control name"
+          type="text"
+          placeholder="name"
+          value={name}
+          name="name"
+          idName="floatingName"
+          onChange={handleName}
+        ></input>
+        <label for="floatingName">name</label>
+      </div>
+      <div className='form-floating mb-3'>
+        <input
+          className="form-control email"
+          type="email"
+          placeholder="email"
+          value={email}
+          name="email"
+          idName="floatingEmail"
+          onChange={handleEmail}
+        ></input>
+        <label for="floatingEmail">email</label>
+      </div>
+      <div className='form-floating mb-3'>
+        <textarea
+          className="form-control message"
+          type="text"
+          placeholder="what can I do for you?"
+          value={message}
+          name="message"
+          idName="floatingMessage" 
+          style={styles.textarea}         
+          onChange={handleMessage}
+        ></textarea>
+          <label for="floatingMessage">what can I do for you?</label>
+      </div>
+      <button type="submit" className="btn btn-primary" idName="submit-button">Submit</button>
+    </form>
+    </div>
+  )
+};
+
+export default ContactForm;
