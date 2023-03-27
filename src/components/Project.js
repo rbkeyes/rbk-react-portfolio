976ca21a262961d1e8f884c24e0681b1c512f266
Reed Keyes <rbkeyes@gmail.com>
2023-03-27 09:59:54 -0700

    card hover working as desired

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,6 +4,18 @@ import { FaGithub } from "react-icons/fa";
 
 function Project( {...props} ) {
 
+    const styles = {
+        card: {
+            "padding": "1rem",
+            "margin": "1rem",
+            "backgroundColor": "#e6e7e8"
+        },
+        link: { 
+            "color": "#e6e7e8"
+        }
+        
+    }
+
     const [onHover, setHover] = useState(false);
 
     return (
@@ -13,28 +25,30 @@ function Project( {...props} ) {
                 key={props.project.title}
                 onMouseEnter={(e) => setHover(true)}
                 onMouseLeave={(e) => setHover(false)}
-                style={{ "padding": "1rem", "margin": "2rem" }}>
+                style={styles.card}>
                 <img
                     className="card-img img-fluid"
                     src={window.location.origin + props.project.path}
                     alt='Screenshot of deployed project'
-                    style={{ "max-height": "30rem", "objectFit": "cover", "objectPosition": "top" }}
                 ></img>
                 {onHover &&
                     <div
                         className='card-img-overlay d-flex flex-column justify-content-end align-items-start bg-dark'
                         style={{ '--bs-bg-opacity': .75, "padding": "2rem" }}>
-                        <a href={props.project.link} style={{ "textDecoration": "none", "color": "#E7E4E8" }}>
+                            <div className='mb-5 mr-2'>
+                        <a className='link-light' href={props.project.link}>
                             <h3 className="card-title mb-4">{props.project.title}</h3></a>
                         <IconContext.Provider value={{ className: "card-icon", size: "3rem" }}>
                             <a
+                                className='link-light'
                                 href={props.project.github}
                                 alt="Link to github profile"
-                                style={{ "color": "#E7E4E8" }}><FaGithub
+                                ><FaGithub
                                 />
                             </a>
                         </IconContext.Provider>
-                    </div>
+                        </div>
+                        </div>
                 }
             </div>
         </div>
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,25 +1,25 @@
 const projects = [
     {
         title: 'Craft But Not Least',
-        path: '/images/craft-but-not-least.herokuapp.com_.png',
+        path: '/images/craft-but-not-least-screenshot.png',
         link: 'https://craft-but-not-least.herokuapp.com/',
         github: 'https://github.com/rbkeyes/craft-but-not-least'
     },
     {
         title: 'Blog About It',
-        path: '/images/blog-about-it.herokuapp.com_.png',
+        path: '/images/blog-about-it-screenshot.png',
         link: 'https://blog-about-it.herokuapp.com/',
         github: 'https://github.com/rbkeyes/blog-about-it'
     },
     {
         title: 'Take Some Notes',
-        path: '/images/take-some-notes.herokuapp.com_notes.png',
+        path: '/images/take-some-notes-screenshot.png',
         link: 'https://take-some-notes.herokuapp.com/',
         github: 'https://github.com/rbkeyes/take-some-notes'
     },
     {
         title: 'Task Tracker',
-        path: '/images/rbkeyes.github.io_task-tracker-2000_.png',
+        path: '/images/task-tracker-2000-screenshot.png',
         link: 'https://rbkeyes.github.io/task-tracker-2000/',
         github: 'https://github.com/rbkeyes/task-tracker-2000'
     }
