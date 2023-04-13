697726b76f36cddbb043df8e301113bca168598d
Reed Keyes <rbkeyes@gmail.com>
2023-04-12 23:24:18 -0700

    resume page styled

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -22,13 +22,110 @@ export default function Resume() {
       <section className="resume">
         <h2>Resume</h2>
         <div>
-          <div class="resume-header">
-            <h5>Reed Keyes</h5>
+
+          <div className="res-header mt-3">
+            <h3>Reed Keyes</h3>
             <p>650-400-2394</p>
             <p>rbkeyes@gmail.com</p>
           </div>
-          <div class="experience">
-            
+
+          <div className="summary">
+            <h3>Summary</h3>
+            <p>Polished blurb about me</p>
+          </div>
+
+          <div className="experience">
+            <h3>Experience</h3>
+            <div className="res-flex d-flex justify-content-between mt-3">
+              <div className="justify-content-start">
+                <h4>Student: Full Stack Web Developer</h4>
+                <h4>October 2022 - present</h4>
+              </div>
+              <div>
+
+              </div>
+              <div className="justify-content-end">
+                <ul>
+                  <li>Soon to complete boot camp</li>
+                  <li>Add more info here</li>
+                </ul>
+              </div>
+            </div>
+            <div className="res-flex d-flex justify-content-between mt-2">
+              <div className="justify-content-start">
+                <h4>Knitting Technical Editor and Knitwear Designer</h4>
+                <h4>March 2019 - present</h4>
+              </div>
+              <div className="justify-content-end">
+                <ul>
+                  <li>Placeholder text</li>
+                  <li>Will add more details after resume has been updated</li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="res-flex d-flex justify-content-between mt-2">
+              <div className="justify-content-start">
+                <h4>Registered Dietitian</h4>
+                <h4>April 2008 - August 2022</h4>
+              </div>
+              <div className="justify-content-end">
+                <ul>
+                  <li>Add some details here</li>
+                  <li>More info about my experience</li>
+                </ul>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+        <div className="education my-3">
+          <h3>Education</h3>
+
+          <div className="res-flex d-flex justify-content-between mt-3">
+            <div className="justify-content-start">
+              <h4>UC Berkeley Extension</h4>
+              <h4>October 2022 - April 2023</h4>
+            </div>
+            <div className="justify-content-end">
+              <ul>
+                <li>Coding boot camp</li>
+              </ul>
+            </div>
+          </div>
+          <div className="res-flex d-flex justify-content-between mt-2">
+            <div className="justify-content-start">
+              <h4>Learn To Tech Edit - The Tech Editor Hub</h4>
+              <h4>March 2019-April 2019</h4>
+            </div>
+            <div className="justify-content-end mt-2">
+              <ul>
+                <li>Online self-paced course</li>
+              </ul>
+            </div>
+          </div>
+          <div className="res-flex d-flex justify-content-between mt-2">
+            <div className="justify-content-start">
+              <h4>East Carolina University</h4>
+              <h4>August 2006-May 2007</h4>
+            </div>
+            <div className="justify-content-end">
+              <ul>
+                <li>Dietetic Internship</li>
+              </ul>
+            </div>
+          </div>
+          <div className="res-flex d-flex justify-content-between mt-2">
+            <div className="justify-content-start">              <h4>University of California Berkeley</h4>
+              <h4>2002-2006</h4>
+            </div>
+            <div className="justify-content-end">
+              <ul>
+                <li>B.S. Nutrition and Dietetics</li>
+                <li>Modern Dance Minor</li>
+              </ul>
+            </div>
           </div>
         </div>
         <div className="btnDiv">
