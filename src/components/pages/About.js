2f3a0a049d07a17201bb458fffd4f182311aa95b
Reed Keyes <rbkeyes@gmail.com>
2023-04-13 11:39:58 -0700

    updated headshot

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import headshot from '../../images/headshot.jpg'
+
 export default function About() {
   return (
     <main>
@@ -8,6 +10,12 @@ export default function About() {
       Reed is a registered dietitian turned web-developer living in the Bay Area. She will be completing the U.C. Berkeley Extensions coding boot camp in mid-March. I need to write a more complete bio but for the sake of turning this in, here's some filler text. 
       Etsy cray biodiesel mustache, vibecession DSA artisan marxism copper mug neutral milk hotel squid normcore cardigan glossier. Next level deep v 8-bit neutral milk hotel mumblecore cupping umami. Vegan aesthetic cardigan, DSA listicle letterpress coloring book fashion axe semiotics vaporware godard mustache pabst.
       </p>
+      <div>
+        <img 
+        className="headshot" 
+        src={headshot} alt="headshot" 
+        style={{width: "250px", height: "250px", objectFit: "cover", borderRadius: "50%"}}/>
+      </div>
     </main>
   );
 }
